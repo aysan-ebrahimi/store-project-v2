b89df18c3a036aa61b51bc631643e0e4a5addd7d
Aysan Ebrahimi <aysanebrahimi1998@gmail.com>
2025-02-09 21:30:36 +0330

    store is made

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@redux/toolkit";
+import productReducer from "../features/product/productSlice";
+
+const store = configureStore({
+  reducer: {
+    product: productReducer,
+  },
+});
+
+export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import store from "./app/store.js";
 import App from "./App.jsx";
 import "./global.css";
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
+  </BrowserRouter>
 );
